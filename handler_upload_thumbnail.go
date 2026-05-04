641261bcda92ad1f23574fc641f2f8d367645dcd
fred-bonn <sadpantz@gmail.com>
2026-05-04 23:06:23 +0200

    first couple of lessons

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,49 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error parsing multipart form", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	multiFile, multiHeader, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error retrieving thumbnail file", err)
+		return
+	}
+	defer multiFile.Close()
+
+	mediaType := multiHeader.Header.Get("Content-Type")
+	fileData, err := io.ReadAll(multiFile)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error reading thumbnail file", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find video metadata", err)
+		return
+	}
+
+	thumbnail := thumbnail{
+		data:      fileData,
+		mediaType: mediaType,
+	}
+
+	videoThumbnails[videoID] = thumbnail
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	video.ThumbnailURL = &thumbnailURL
+	fmt.Println(thumbnailURL)
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error updating video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
